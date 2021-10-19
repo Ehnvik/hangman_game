f3ab97861c1eff542f3b335de17bb0bf2504f84a
Ehnvik <gustav.ehnvik@gmail.com>
2021-10-19 11:20:25 +0000

    add words to words.py

---
+++ words.py
@@ -0,0 +1,3 @@
+words = ["allow", "monkey", "car", "amazing", "lucky", "house",
+         "television", "football", "fly", "drive", "company", "school"
+         "train", "waiting", "yellow", "reason", "milk", "polite"]
